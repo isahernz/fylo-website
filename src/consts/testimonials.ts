15d78636136e4383c47de319df28867dde031c95
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2024-07-29 20:51:42 -0600

    feat: Add testimonials to the website

---
+++ src/consts/testimonials.ts
@@ -0,0 +1,20 @@
+export const TESTIMONIALS = [
+  {
+    name: "Sarish Patel",
+    text: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+    position: "Founder & CEO, Huddle",
+    avatar: "/avatars/profile-1.jpg"
+  },
+  {
+    name: "Sarish Patel",
+    text: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+    position: "Founder & CEO, Huddle",
+    avatar: "/avatars/profile-2.jpg"
+  },
+  {
+    name: "Sarish Patel",
+    text: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+    position: "Founder & CEO, Huddle",
+    avatar: "/avatars/profile-3.jpg"
+  }
+]
